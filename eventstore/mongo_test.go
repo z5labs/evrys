f9b4b7b227ffc99c6b47447c4da59dbeef4ff75e
Eric Solender <ericsolender@gmail.com>
2022-11-08 15:12:10 -0500

    finished mongo test

--- eventstore/mongo_test.go
+++ eventstore/mongo_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/wait"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -183,7 +184,8 @@ func TestMongoIntegration(t *testing.T) {
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
 	req.NoError(err, "failed to connect to mongo")
 
-	req.NoError(client.Ping(ctx, readpref.Primary()), "failed to ping mongo")
+	err = client.Ping(ctx, readpref.Primary())
+	req.NoError(err, "failed to ping mongo")
 
 	// impl setup
 	db := "testdb"
@@ -203,16 +205,18 @@ func TestMongoIntegration(t *testing.T) {
 	// data setup
 	_event := event.New()
 	id := "some_random_id"
+	curTime := time.Now().UTC()
 	_event.SetID(id)
 	_event.SetSubject("test")
 	_event.SetSource("mongo_test")
-	_event.SetTime(time.Now())
+	_event.SetTime(curTime)
 	_event.SetSpecVersion(event.CloudEventsVersionV1)
 	_event.SetType("test")
 	_event.SetData(*event.StringOfApplicationJSON(), map[string]interface{}{"hello": "world"})
 
 	// actual test
-	req.NoError(mongoImpl.PutEvent(ctx, &_event), "failed to put event")
+	err = mongoImpl.PutEvent(ctx, &_event)
+	req.NoError(err, "failed to put event")
 
 	coll := client.Database(db).Collection(collName)
 	filter := bson.D{{Key: "id", Value: id}}
@@ -220,7 +224,51 @@ func TestMongoIntegration(t *testing.T) {
 	req.NoError(err, "failed to find record")
 
 	var results []bson.D
-	req.NoError(cursor.All(ctx, &results), "failed to convert to array")
+	err = cursor.All(ctx, &results)
+	req.NoError(err, "failed to convert to array")
 
 	req.Len(results, 1, "returned slice not of correct length")
+
+	result := results[0]
+	var idv, spec, source, _type, sub, dct, tm, dt = false, false, false, false, false, false, false, false
+	for key, value := range result.Map() {
+		switch key {
+		case "id":
+			req.Equal(id, value, "id not expected value")
+			idv = true
+			continue
+		case "specversion":
+			req.Equal("1.0", value, "spec version not expected value")
+			spec = true
+			continue
+		case "source":
+			req.Equal("mongo_test", value, "source not expected value")
+			source = true
+			continue
+		case "type":
+			req.Equal("test", value, "type not expected value")
+			_type = true
+			continue
+		case "subject":
+			req.Equal("test", value, "subject not expected value")
+			sub = true
+			continue
+		case "datacontenttype":
+			req.Equal("application/json", value, "datacontenttype not expected value")
+			dct = true
+			continue
+		case "time":
+			req.Equal(curTime.Format(time.RFC3339Nano), value, "time is not equal")
+			tm = true
+			continue
+		case "data":
+			d := value.(primitive.D)
+			v := d[0]
+			req.Equal("hello", v.Key, "key not expected value")
+			req.Equal("world", v.Value, "value not of expected value")
+			dt = true
+			continue
+		}
+	}
+	req.True(idv && spec && source && _type && sub && dct && tm && dt, "all values have not been verified")
 }
