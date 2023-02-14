b8f6db73a971c30ef38886d6d9d2fceb4148b9a6
Richard Carson Derr <cakub6@gmx.com>
2023-02-14 11:06:16 -0500

    story(grpc): implement Append rpc (#68)
    * feat(svc-event-log): implement public serve function
    
    * feat(svc-event-log): implemented and tested append rpc
    
    * lint(svc-event-log): fixed import format
    
    * feat(svc-event-log): added log to address review comment

--- svc-event-log/grpc/service.go
+++ svc-event-log/grpc/service.go
@@ -16,24 +16,130 @@ package grpc
 
 import (
 	"context"
+	"errors"
+	"net"
 
+	"github.com/z5labs/evrys/lib/eventstore"
 	"github.com/z5labs/evrys/svc-event-log/eventlogpb"
 
+	format "github.com/cloudevents/sdk-go/binding/format/protobuf/v2"
+	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
+// EventStore
+type EventStore interface {
+	eventstore.AppendOnly
+}
+
+// ServiceConfig
+type ServiceConfig struct {
+	Logger     *zap.Logger
+	EventStore EventStore
+	Listener   net.Listener
+}
+
 // Serve
-func Serve(ctx context.Context) error {
-	return nil
+func Serve(ctx context.Context, cfg ServiceConfig) error {
+	if cfg.EventStore == nil {
+		return errors.New("event store must be provided")
+	}
+	if cfg.Listener == nil {
+		return errors.New("listener must be set")
+	}
+	s := &service{
+		log:   cfg.Logger,
+		store: cfg.EventStore,
+	}
+	if s.log == nil {
+		s.log = zap.NewNop()
+	}
+
+	grpcServer := grpc.NewServer()
+	eventlogpb.RegisterEventLogServer(grpcServer, s)
+
+	done := make(chan struct{}, 1)
+	g, gctx := errgroup.WithContext(ctx)
+	g.Go(func() (err error) {
+		defer close(done)
+		return grpcServer.Serve(cfg.Listener)
+	})
+	g.Go(func() error {
+		select {
+		case <-gctx.Done():
+			grpcServer.GracefulStop()
+			return gctx.Err()
+		case <-done:
+			return nil
+		}
+	})
+
+	err := g.Wait()
+	<-done
+	return err
 }
 
 type service struct {
 	eventlogpb.UnimplementedEventLogServer
+
+	log   *zap.Logger
+	store EventStore
 }
 
 // Append
 func (s *service) Append(ctx context.Context, req *eventlogpb.AppendRequest) (*emptypb.Empty, error) {
-	return nil, nil
+	if req.Event == nil {
+		s.log.Warn("client attempted to append nil cloudevent to log")
+		return nil, status.Error(codes.InvalidArgument, "cloudevent must be non-nil")
+	}
+
+	ev, err := format.FromProto(req.Event)
+	if err != nil {
+		s.log.Error("failed to convert cloudevent protobuf to generic cloudevent")
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+	s.log.Debug(
+		"received event to append to log",
+		zap.String("event_id", ev.ID()),
+		zap.String("event_type", ev.Type()),
+		zap.String("event_source", ev.Source()),
+	)
+
+	err = ev.Validate()
+	if err != nil {
+		s.log.Error(
+			"received invalid cloudevent",
+			zap.String("event_id", ev.ID()),
+			zap.String("event_type", ev.Type()),
+			zap.String("event_source", ev.Source()),
+			zap.Error(err),
+		)
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	err = s.store.Append(ctx, ev)
+	if err != nil {
+		s.log.Error(
+			"failed to append cloudevent to log",
+			zap.String("event_id", ev.ID()),
+			zap.String("event_type", ev.Type()),
+			zap.String("event_source", ev.Source()),
+			zap.Error(err),
+		)
+		return nil, status.Error(codes.Unavailable, err.Error())
+	}
+	s.log.Debug(
+		"appended event to log",
+		zap.String("event_id", ev.ID()),
+		zap.String("event_type", ev.Type()),
+		zap.String("event_source", ev.Source()),
+	)
+
+	return &emptypb.Empty{}, nil
 }
 
 // Iterate
---
+++ svc-event-log/grpc/service_test.go
@@ -0,0 +1,417 @@
+// Copyright 2023 Z5Labs and Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package grpc
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"testing"
+	"time"
+
+	"github.com/z5labs/evrys/svc-event-log/eventlogpb"
+
+	"github.com/cloudevents/sdk-go/binding/format/protobuf/v2/pb"
+	"github.com/cloudevents/sdk-go/v2/event"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
+)
+
+type mockEventStore struct {
+	append func(context.Context, *event.Event) error
+}
+
+func (s mockEventStore) Append(ctx context.Context, ev *event.Event) error {
+	return s.append(ctx, ev)
+}
+
+func ExampleServe() {
+	ls, err := net.Listen("tcp", ":0")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	cfg := ServiceConfig{
+		EventStore: mockEventStore{},
+		Listener:   ls,
+	}
+
+	err = Serve(ctx, cfg)
+	if err != context.DeadlineExceeded {
+		fmt.Println(err)
+		return
+	}
+	// Output:
+}
+
+type mockListener struct {
+	accept func() (net.Conn, error)
+	close  func() error
+	addr   func() net.Addr
+}
+
+func (l mockListener) Accept() (net.Conn, error) {
+	return l.accept()
+}
+
+func (l mockListener) Close() error {
+	return l.close()
+}
+
+func (l mockListener) Addr() net.Addr {
+	return l.addr()
+}
+
+func TestServe(t *testing.T) {
+	t.Run("will return an error", func(t *testing.T) {
+		t.Run("if no event store is provided", func(t *testing.T) {
+			ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+			defer cancel()
+
+			cfg := ServiceConfig{
+				Logger:   zap.NewNop(),
+				Listener: &net.TCPListener{},
+			}
+			err := Serve(ctx, cfg)
+			if !assert.Error(t, err) {
+				return
+			}
+		})
+
+		t.Run("if no listener is provided", func(t *testing.T) {
+			ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+			defer cancel()
+
+			cfg := ServiceConfig{
+				Logger:     zap.NewNop(),
+				EventStore: mockEventStore{},
+			}
+			err := Serve(ctx, cfg)
+			if !assert.Error(t, err) {
+				return
+			}
+		})
+
+		t.Run("if the listener fails to accept a connection", func(t *testing.T) {
+			ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+			defer cancel()
+
+			acceptErr := errors.New("accept failed")
+			cfg := ServiceConfig{
+				Logger:     zap.NewNop(),
+				EventStore: mockEventStore{},
+				Listener: mockListener{
+					accept: func() (net.Conn, error) {
+						return nil, acceptErr
+					},
+					close: func() error { return nil },
+					addr:  func() net.Addr { return &net.IPAddr{} },
+				},
+			}
+			err := Serve(ctx, cfg)
+			if !assert.Equal(t, acceptErr, err) {
+				return
+			}
+		})
+	})
+}
+
+func TestService_Append(t *testing.T) {
+	t.Run("will return an error", func(t *testing.T) {
+		t.Run("if no cloudevent is provided in the request", func(t *testing.T) {
+			ls, err := net.Listen("tcp", ":0")
+			if !assert.Nil(t, err) {
+				return
+			}
+
+			errCh := make(chan error, 1)
+			defer func() {
+				err := <-errCh
+				if !assert.ErrorIs(t, err, context.Canceled) {
+					return
+				}
+			}()
+
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+			go func() {
+				defer close(errCh)
+				err := Serve(ctx, ServiceConfig{
+					EventStore: mockEventStore{},
+					Listener:   ls,
+				})
+				errCh <- err
+			}()
+
+			cc, err := grpc.Dial(ls.Addr().String(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+			if !assert.Nil(t, err) {
+				return
+			}
+			defer cc.Close()
+
+			client := eventlogpb.NewEventLogClient(cc)
+
+			req := &eventlogpb.AppendRequest{}
+			_, err = client.Append(ctx, req)
+			if !assert.Error(t, err) {
+				return
+			}
+
+			s, ok := status.FromError(err)
+			if !assert.True(t, ok) {
+				t.Log(err)
+				return
+			}
+			if !assert.Equal(t, codes.InvalidArgument, s.Code()) {
+				return
+			}
+		})
+
+		t.Run("if no protobuf cloudevent can not be mapped to an encoding agnostic model", func(t *testing.T) {
+			ls, err := net.Listen("tcp", ":0")
+			if !assert.Nil(t, err) {
+				return
+			}
+
+			errCh := make(chan error, 1)
+			defer func() {
+				err := <-errCh
+				if !assert.ErrorIs(t, err, context.Canceled) {
+					return
+				}
+			}()
+
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+			go func() {
+				defer close(errCh)
+				err := Serve(ctx, ServiceConfig{
+					EventStore: mockEventStore{},
+					Listener:   ls,
+				})
+				errCh <- err
+			}()
+
+			cc, err := grpc.Dial(ls.Addr().String(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+			if !assert.Nil(t, err) {
+				return
+			}
+			defer cc.Close()
+
+			client := eventlogpb.NewEventLogClient(cc)
+
+			ev := &pb.CloudEvent{
+				Data: &pb.CloudEvent_TextData{
+					TextData: "",
+				},
+				Attributes: map[string]*pb.CloudEventAttributeValue{
+					"datacontenttype": {
+						Attr: &pb.CloudEventAttributeValue_CeString{
+							CeString: "json",
+						},
+					},
+				},
+			}
+			req := &eventlogpb.AppendRequest{Event: ev}
+			_, err = client.Append(ctx, req)
+			if !assert.Error(t, err) {
+				return
+			}
+
+			s, ok := status.FromError(err)
+			if !assert.True(t, ok) {
+				t.Log(err)
+				return
+			}
+			if !assert.Equal(t, codes.InvalidArgument, s.Code()) {
+				return
+			}
+		})
+
+		t.Run("if the cloudevent is invalid", func(t *testing.T) {
+			ls, err := net.Listen("tcp", ":0")
+			if !assert.Nil(t, err) {
+				return
+			}
+
+			errCh := make(chan error, 1)
+			defer func() {
+				err := <-errCh
+				if !assert.ErrorIs(t, err, context.Canceled) {
+					return
+				}
+			}()
+
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+			go func() {
+				defer close(errCh)
+				err := Serve(ctx, ServiceConfig{
+					EventStore: mockEventStore{},
+					Listener:   ls,
+				})
+				errCh <- err
+			}()
+
+			cc, err := grpc.Dial(ls.Addr().String(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+			if !assert.Nil(t, err) {
+				return
+			}
+			defer cc.Close()
+
+			client := eventlogpb.NewEventLogClient(cc)
+
+			ev := &pb.CloudEvent{}
+			req := &eventlogpb.AppendRequest{Event: ev}
+			_, err = client.Append(ctx, req)
+			if !assert.Error(t, err) {
+				return
+			}
+
+			s, ok := status.FromError(err)
+			if !assert.True(t, ok) {
+				t.Log(err)
+				return
+			}
+			if !assert.Equal(t, codes.InvalidArgument, s.Code()) {
+				return
+			}
+		})
+
+		t.Run("if the event store implementation fails to append", func(t *testing.T) {
+			ls, err := net.Listen("tcp", ":0")
+			if !assert.Nil(t, err) {
+				return
+			}
+
+			errCh := make(chan error, 1)
+			defer func() {
+				err := <-errCh
+				if !assert.ErrorIs(t, err, context.Canceled) {
+					return
+				}
+			}()
+
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+			go func() {
+				defer close(errCh)
+				err := Serve(ctx, ServiceConfig{
+					EventStore: mockEventStore{
+						append: func(ctx context.Context, e *event.Event) error {
+							return errors.New("append failed")
+						},
+					},
+					Listener: ls,
+				})
+				errCh <- err
+			}()
+
+			cc, err := grpc.Dial(ls.Addr().String(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+			if !assert.Nil(t, err) {
+				return
+			}
+			defer cc.Close()
+
+			client := eventlogpb.NewEventLogClient(cc)
+
+			ev := &pb.CloudEvent{
+				Id:          "123",
+				Type:        "test",
+				Source:      "test",
+				SpecVersion: "1.0",
+			}
+			req := &eventlogpb.AppendRequest{Event: ev}
+			_, err = client.Append(ctx, req)
+			if !assert.Error(t, err) {
+				return
+			}
+
+			s, ok := status.FromError(err)
+			if !assert.True(t, ok) {
+				t.Log(err)
+				return
+			}
+			if !assert.Equal(t, codes.Unavailable, s.Code()) {
+				t.Log(err)
+				return
+			}
+		})
+	})
+
+	t.Run("will return an empty response", func(t *testing.T) {
+		t.Run("if the event is valid and the event store append operation is successful", func(t *testing.T) {
+			ls, err := net.Listen("tcp", ":0")
+			if !assert.Nil(t, err) {
+				return
+			}
+
+			errCh := make(chan error, 1)
+			defer func() {
+				err := <-errCh
+				if !assert.ErrorIs(t, err, context.Canceled) {
+					return
+				}
+			}()
+
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+			go func() {
+				defer close(errCh)
+				err := Serve(ctx, ServiceConfig{
+					EventStore: mockEventStore{
+						append: func(ctx context.Context, e *event.Event) error {
+							return nil
+						},
+					},
+					Listener: ls,
+				})
+				errCh <- err
+			}()
+
+			cc, err := grpc.Dial(ls.Addr().String(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+			if !assert.Nil(t, err) {
+				return
+			}
+			defer cc.Close()
+
+			client := eventlogpb.NewEventLogClient(cc)
+
+			ev := &pb.CloudEvent{
+				Id:          "123",
+				Type:        "test",
+				Source:      "test",
+				SpecVersion: "1.0",
+			}
+			req := &eventlogpb.AppendRequest{Event: ev}
+			resp, err := client.Append(ctx, req)
+			if !assert.Nil(t, err) {
+				return
+			}
+			if !assert.NotNil(t, resp) {
+				return
+			}
+		})
+	})
+}
